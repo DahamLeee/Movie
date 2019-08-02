891997f366f352c382c9a3ad6aca1b72938c9df4
DahamLeee <dlek1011@gmail.com>
2019-08-03 01:18:03 +0900

    proj2 Ver-final

--- proj2/app/src/main/java/com/example/proj2/MainActivity.java
+++ proj2/app/src/main/java/com/example/proj2/MainActivity.java
@@ -7,16 +7,62 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ReviewAdapter reviewAdapter;
     RecyclerView recyclerView;
+    TextView likeCount;
+    TextView hateCount;
+    ImageButton like;
+    ImageButton hate;
+    String temp = null;
+    int temp1 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        likeCount = findViewById(R.id.likeCount);
+        hateCount = findViewById(R.id.hateCount);
+
+        like = findViewById(R.id.like);
+        like.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                like.setSelected(!like.isSelected());
+                if(like.isSelected()){
+                    if(hate.isSelected()){
+                        hate.setSelected(!hate.isSelected());
+                        hateDecrease();
+                    }
+                    likeIncrease();
+                }
+                else{
+                    likeDecrease();
+                }
+            }
+        });
+        hate = findViewById(R.id.hate);
+        hate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                hate.setSelected(!hate.isSelected());
+                if(hate.isSelected()){
+                    if(like.isSelected()){
+                        like.setSelected(!like.isSelected());
+                        likeDecrease();
+                    }
+                    hateIncrease();
+                }
+                else{
+                    hateDecrease();
+                }
+            }
+        });
 
         recyclerView = findViewById(R.id.recyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
@@ -43,13 +89,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        new Thread(new Runnable(){
-//            @Override
-//            public void run() {
-//                layout2 = findViewById(R.id.layout2);
-//                layout2.setRate("5위  1.8%");
-//                layout2.setClientCount("839,399명");
-//            }
-//        }).start();
+    }
+    public void likeIncrease(){
+        temp = likeCount.getText().toString();
+        temp1 = Integer.parseInt(temp);
+        temp1 += 1;
+        temp = String.valueOf(temp1);
+        likeCount.setText(temp);
+    }
+    public void likeDecrease(){
+        temp = likeCount.getText().toString();
+        temp1 = Integer.parseInt(temp);
+        temp1 -= 1;
+        temp = String.valueOf(temp1);
+        likeCount.setText(temp);
+    }
+    public void hateIncrease(){
+        temp = hateCount.getText().toString();
+        temp1 = Integer.parseInt(temp);
+        temp1 += 1;
+        temp = String.valueOf(temp1);
+        hateCount.setText(temp);
+    }
+    public void hateDecrease(){
+        temp = hateCount.getText().toString();
+        temp1 = Integer.parseInt(temp);
+        temp1 -= 1;
+        temp = String.valueOf(temp1);
+        hateCount.setText(temp);
     }
 }
