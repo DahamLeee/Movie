711869048f71ecdd128b46c7d108cefc66786f8e
DahamLeee <dlek1011@gmail.com>
2019-08-08 03:56:32 +0900

    proj4 Ver1

---
+++ proj4/app/src/main/java/com/example/proj4/Fragment1.java
@@ -0,0 +1,31 @@
+package com.example.proj4;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class Fragment1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment1, container, false);
+
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "hello",Toast.LENGTH_LONG ).show();
+            }
+        });
+        return view;
+    }
+}
---
+++ proj4/app/src/main/java/com/example/proj4/Sample.java
@@ -0,0 +1,22 @@
+package com.example.proj4;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Sample extends Fragment {
+    public static Sample newInstance(){
+        return new Sample();
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.sample, container, false);
+
+        return view;
+    }
+}
